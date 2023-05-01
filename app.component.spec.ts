758f6f6c924247b8198c21122e1980d3de2c94ea
Gaurav pawar <91130415+gap1997@users.noreply.github.com>
2023-05-01 12:11:47 +0530

    assingment 8
    assingment 8

--- app.component.spec.ts
+++ app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'assinment7.4'`, () => {
+  it(`should have as title 'ass8'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('assinment7.4');
+    expect(app.title).toEqual('ass8');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('assinment7.4 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ass8 app is running!');
   });
 });
--- app.module.ts
+++ app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
-import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './child/child.component';
+
 @NgModule({
-  declarations: [AppComponent, DemoComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    ChildComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
