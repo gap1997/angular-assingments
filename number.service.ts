e8263f6d484b3dda4291ee545f609901e6fe8d9f
Gaurav pawar <91130415+gap1997@users.noreply.github.com>
2023-05-01 12:04:54 +0530

    assinment
    assingnment 9

---
+++ number.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NumberService {
+  constructor() {}
+  ChkPrime(A: number): boolean {
+    if (A <= 1) {
+      return false;
+    }
+    for (let i = 2; i < A; i++) {
+      if (A % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
---
+++ string.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StringService {
+  constructor() {}
+
+  CountCapital(str: string) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str.charAt(i) >= 'A' && str.charAt(i) <= 'z') {
+        count++;
+      }
+    }
+    return count;
+  }
+}
